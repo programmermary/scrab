1fbaefe7ab4e8e54555e44c04c05e9bc83070f4d
maryam_ihsan <maryamihsan601@gmail.com>
2025-02-12 22:48:16 +0300

    add links to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import { navbarData } from '../Data/Navbar'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 function Navbar() {
   return (
     <>
     <nav>
       <div className="container">
+        <div>
+         <FavoriteBorderIcon/>
+         <AddShoppingCartIcon/>
+         <PersonOutlineIcon/>
+        </div>
+
+        <div>
+          <ul>
+            {
+              navbarData.map((item)=>{
+                return <li key={item.id} >
+                  <a href={item.link}>{item.title}</a>
+                </li>
+              })
+            }
+          </ul>
+        </div>
         {/*---------------logo section----------------------------------------*/}
         <div className='text-2xl flex items-center gap-2 font-bold py-8'> 
           <p><span className='text-4xl text-first'>S</span>craby</p>
