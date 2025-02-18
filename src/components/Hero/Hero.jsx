b516786032c54f71cf7b59a9fe89cd7141b8a802
maryam_ihsan <maryamihsan601@gmail.com>
2025-02-18 19:49:50 +0300

    style hero section for mobile view

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
-
+import femaleDoctorWithLabCoat from '../../assets/femaleDoctorWithLabCoat.png'
 const Hero = () => {
   return (
     <section>
         <div className="container w-screen h-screen gird grid-cols-1 md:grid-cols-2 ">
-          {/* <div className=' text-center md:text-right space-y-6 flex '>
-          <div className='flex flex-col justify-center py-14 md:py-0'>
-               
-                
-            </div>
-           
-          </div> */}
-          <div className="main h-full w-full  flex ">
-          <div className="image bg-black w-1/2">
-              this is image div
+     
+          <div className="main h-full w-full  flex flex-col md:flex-row items-center">
+          <div className="image hidden md:block w-1/2 h-3/4 ">
+              <img src={femaleDoctorWithLabCoat} alt="" />
             </div>
             <div className="text w-1/2 flex flex-col 
-            justify-around p-1 text-right">
-                <div className="innerText">
-                <h1 className='text-3xl font-bold leading-relaxed  
+            justify-center gap-8 p-1 text-right">
+                <div className="innerText space-y-6">
+                <h1 className='text-3xl md:text-5xl font-bold leading-relaxed  
                  xl-leading-normal text-right font-Alexandria'
                  >احصل على <span className=' text-first '
                  >سكرابات</span> طبية عالية الجودة الآن</h1>
@@ -29,7 +23,7 @@ const Hero = () => {
                </p>
                 </div>
                <button className='text-first border-first border p-3
-                rounded hover:text-white hover:bg-first self-right w-1/2 self-center'>
+                rounded hover:text-white hover:bg-first self-right w-full md:w-1/4 self-center'>
                   تسوق الان</button>
             </div>
             
