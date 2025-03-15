8491544c3a963270c28a46f49fa9949cf9e0e4c3
maryam_ihsan <maryamihsan601@gmail.com>
2025-03-15 17:17:52 +0300

    connect to db

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -13,7 +13,7 @@ const RegistrationPage =() => {
   const handelSubmit = async (e) => {
   e.preventDefault()
   try{
-    const reponse = await axios.post('mongodb+srv://maryamihsan601:iNOAaEDBUUxWlLDk@cluster0.8td0dum.mongodb.net/ScrabDataBase' ,{
+    const reponse = await axios.post('mongodb+srv://maryamihsan601:iNOAaEDBUUxWlLDk@cluster0.8td0dum.mongodb.net/ScrabDataBase/users' ,{
       name,
       email,
       password
